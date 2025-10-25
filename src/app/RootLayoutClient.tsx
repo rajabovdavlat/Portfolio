5aae46edebc767368b14b405b6c53c6b6b3e6cc7
rajabovdavlat <rajabovoff1@gmail.com>
2025-10-25 12:20:15 +0500

    Removed MatrixIntro import and usage

--- src/app/RootLayoutClient.tsx
+++ src/app/RootLayoutClient.tsx
@@ -2,7 +2,6 @@
 import { LanguageProvider } from "@/context/language-context";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import MatrixIntro from "@/components/MatrixIntro";
 
 export default function RootLayoutClient({
   children,
